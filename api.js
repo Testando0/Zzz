9621334b81935364480d1401786c0e25415318b5
Testando0 <joh769637@gmail.com>
2025-07-12 15:52:49 -0300

    Update api.js

--- api.js
+++ api.js
@@ -6,7 +6,6 @@ const fs = require('fs');
 const path = require('path');
 const JXR = require("jxr-canvas");
 const canvacard = require("canvacard")
-const morgan  = require("morgan");
 const { createDecipheriv } = require("crypto");
 const Canvasfy = require("canvafy")
 const cheerio = require('cheerio');
